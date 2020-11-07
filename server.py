3c1d9298922f3d4b4e8853e247e6061a698971a4
Sam Gillis <sbvb13@gmail.com>
2020-11-07 16:07:40 +0000

    Seeded data and created homepage flask routes

--- server.py
+++ server.py
@@ -15,6 +15,20 @@ app.jinja_env.undefined = StrictUndefined
 api_key = os.environ.get('api_key')
 
 
+@app.route('/')
+def homepage():
+    """View homepage."""
+
+    return render_template('homepage.html')
+
+
+@app.route('/search')
+def search_page():
+    """Takes you to a search bar"""
+
+    return render_template('search.html')
+
+
 if __name__ == '__main__':
     connect_to_db(app)
     app.run(host='0.0.0.0', debug=True)
