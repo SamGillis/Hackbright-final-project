9e305b7d96ddf6d901c7643b870d284421b025c4
Sam Gillis <sbvb13@gmail.com>
2020-11-16 18:13:17 +0000

    updated seed_database to create multiple users

--- seed_database.py
+++ seed_database.py
@@ -13,21 +13,26 @@ os.system('createdb books')
 model.connect_to_db(server.app)
 model.db.create_all()
 
-user = crud.create_user('test@test.com', 'TEST', 'Test!')
-
-collection = crud.create_collection(user, 'home')
-
-with open('tests/data/books.json') as f:
-    book_data = json.loads(f.read())
-
 for i in range(10):
-    book_info = book_data['items'][i]
+    user_email = f'test{i}@test.com'
+    user = crud.create_user(user_email, 'TEST', 'Test!')
+    collection = crud.create_collection(user, 'home')
 
-    google_id, cover_img, title = (book_info['id'],
+    with open('tests/data/books.json') as f:
+        book_data = json.loads(f.read())
+
+    for i in range(10):
+        book_info = book_data['items'][i]
+
+        google_id, cover_img, title = (book_info['id'],
                                     book_info['volumeInfo']['imageLinks']['thumbnail'],
                                     book_info['volumeInfo']['title'])
 
-    book = crud.create_book(google_id, cover_img, title)
-    collection.add_book(book)
+        if crud.Book.query.get(google_id) == None:
+            book = crud.create_book(google_id, cover_img, title)
+        else: 
+            book = crud.Book.query.get(google_id)
+
+        collection.add_book(book)
 
 
