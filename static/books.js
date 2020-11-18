da8599d68749591edfe122bdc54ccaf905c43f21
Sam Gillis <sbvb13@gmail.com>
2020-11-18 01:22:18 +0000

    Blocks user from jumping to an out of bounds page

--- static/books.js
+++ static/books.js
@@ -4,15 +4,22 @@ function jumptopageresults(evt) {
 
     const pageInput = document.querySelector('input[name="page"]');
 
+    let pagesData = $('#pages-java').data('count');
+
+    if (pagesData < pageInput.value) {
+        alert('This page does not exhist')
+        return
+    }
+
     let url = window.location.href;
     let urlComponents = url.split('/');
     url = urlComponents[urlComponents.length - 1];
     urlComponents = url.split("&");
-    let temp = urlComponents.pop()
+    let temp = urlComponents.pop();
     if (!temp.includes('page')) {
-        urlComponents.push(temp)
+        urlComponents.push(temp);
     }
-    url = urlComponents.join('&')
+    url = urlComponents.join('&');
     window.location.href = `/${url}&page=${pageInput.value}`;
 }
 
